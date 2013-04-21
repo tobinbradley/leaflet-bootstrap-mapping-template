5c4ba1709e4aa18375d87e4bb8a8d21c773e201d
tobinbradley <tobin.bradley@gmail.com>
2013-04-21 19:41:55 -0400

    unminified pubsub - minify will occur on build

--- assets/scripts/plugins.js
+++ assets/scripts/plugins.js
@@ -3,8 +3,39 @@
 {console.log();return window.console;}catch(err){return window.console={};}})());
 
 // jQuery pub/sub plugin by Peter Higgins modified by Tobin Bradley
-(function(d){var cache={};d.publish=function(topic,args){cache[topic]&&d.each(cache[topic],function(){try{this.apply(d,args||[]);}catch(err){console.log(err);}});};d.subscribe=function(topic,callback){if(!cache[topic]){cache[topic]=[];}
-cache[topic].push(callback);return[topic,callback];};d.unsubscribe=function(topic,callback){cache[topic]&&d.each(cache[topic],function(idx){if(this==callback){cache[topic].splice(idx,1);}});};d.subscribers=function(topic){l=[];cache[topic]&&d.each(cache[topic],function(idx){l.push(this.name);});return l;};})(jQuery);
+(function (d) {
+    var cache = {};
+    d.publish = function (topic, args) {
+        cache[topic] && d.each(cache[topic], function () {
+            try {
+                this.apply(d, args || []);
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    };
+    d.subscribe = function (topic, callback) {
+        if (!cache[topic]) {
+            cache[topic] = [];
+        }
+        cache[topic].push(callback);
+        return [topic, callback];
+    };
+    d.unsubscribe = function (topic, callback) {
+        cache[topic] && d.each(cache[topic], function (idx) {
+            if (this == callback) {
+                cache[topic].splice(idx, 1);
+            }
+        });
+    };
+    d.subscribers = function (topic) {
+        l = [];
+        cache[topic] && d.each(cache[topic], function (idx) {
+            l.push(this.name);
+        });
+        return l;
+    };
+})(jQuery);
 
 // determine if is number
 function isNumber(n) {
